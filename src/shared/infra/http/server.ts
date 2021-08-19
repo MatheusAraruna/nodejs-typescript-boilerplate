82aa53efd517505d8de8caa60df5c44c1c27dd04
antonio.almeida <antonio.almeida@sotran.com.br>
2021-08-19 07:47:48 -0300

    eslint and prettier configure

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/',(_,res)=>{
     return res.send("hello");
-})
-app.listen(3000);
+});
+const server = http.createServer(app);
+
+server.listen(3000);
